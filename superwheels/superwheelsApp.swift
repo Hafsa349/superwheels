1c083bec1ab98283a7c3f8d69274dd26f900542b
Syed Shariq <syedshariq@live.com>
2023-11-16 13:50:45 +1100

    Initial Commit

---
+++ superwheels/superwheelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  superwheelsApp.swift
+//  superwheels
+//
+//  Created by Syed Shariq on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct superwheelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
